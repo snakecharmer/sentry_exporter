8eb3c82e6fe2e02ca860306363a9e1b6bb314f22
Andreyev Dias de Melo <andreyev@andreyev.net>
2018-06-22 12:07:51 -0300

    WIP

--- http.go
+++ http.go
@@ -43,13 +43,13 @@ func probeHTTP(target string, w http.ResponseWriter, module Module) (success boo
 		return
 	}
 
-  for key, value := range config.Headers {
-    if strings.Title(key) == "Host" {
-      request.Host = value
-      continue
-    }
-    request.Header.Set(key, value)
-  }
+	for key, value := range config.Headers {
+		if strings.Title(key) == "Host" {
+			request.Host = value
+			continue
+		}
+		request.Header.Set(key, value)
+	}
 
 	resp, err := client.Do(request)
 	// Err won't be nil if redirects were turned off. See https://github.com/golang/go/issues/3795
